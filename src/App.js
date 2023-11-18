c6f8ee1d82875ee96a4a9a83d82019b8fe7158b9
root <my email address used on my git repo>
2023-11-18 17:40:53 +0800

    change react desc for docker login

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Testing AWS
+          Edit <code>src/App.js</code> Testing AWS and login
         </p>
         <a
           className="App-link"
