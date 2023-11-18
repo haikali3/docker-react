8487d3c2f861d5be7578e62bca1f66c0487fd371
root <my email address used on my git repo>
2023-11-18 17:37:23 +0800

    change react desc

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>.asdasd
+          Edit <code>src/App.js</code> Testing AWS
         </p>
         <a
           className="App-link"
